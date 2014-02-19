d86f458446c45ff4fe180eefc24184ecb56c63a3
trevmckendrick <trevormckendrick@gmail.com>
2014-02-19 00:23:54 -0700

    image tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,6 @@ module ApplicationHelper
 
   def mountain_time(time)
     converted_time = time.in_time_zone("Mountain Time (US & Canada)").strftime("%I:%M %p")
-    if  converted_time[0] = "0"
-      converted_time[1..-1]
-    else
-      converted_time
-    end
   end
 
   def mountain_date(time)
