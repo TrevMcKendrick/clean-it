042fa2306481508778f30738ccab1fd2a1edf524
trevmckendrick <trevormckendrick@gmail.com>
2014-02-19 12:21:28 -0700

    asset precompilation config changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ CleanIt::Application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
 
-  config.assets.prefix = "/dev-assets"
+  # config.assets.prefix = "/dev-assets"
 
   config.cache_classes = false
 
