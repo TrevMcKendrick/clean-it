4f594d2271af5c7709e5b3bca76a4d57b75a1b0b
trevmckendrick <trevormckendrick@gmail.com>
2014-02-20 17:46:20 -0700

    add twilio texting

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,14 +3,12 @@ protect_from_forgery :except => ["send_text_message"]
 
   def send_text_message
 
-    # params[:from] 
-
     @twilio_client = Twilio::REST::Client.new TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
 
     @twilio_client.account.sms.messages.create(
       :from => TWILIO_PHONE_NUMBER,
       :to => TREVOR_CELL_PHONE,
-      :body => "EverHaven phone call"
+      :body => "EverHaven " + params[:body]
     )
      render inline: "<Response><Dial>6262444636</Dial></Response>", content_type: "text/xml"
    end
